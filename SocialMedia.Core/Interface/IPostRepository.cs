e326c1a6dee109f0c9a1d920bb55d0fcf0cdebd4
Fernando Alfredo Sánchez Valadez <fernandoa.sanchez@softtek.com>
2020-05-25 16:18:33 -0500

    Add project files.

---
+++ SocialMedia.Core/Interface/IPostRepository.cs
@@ -0,0 +1,15 @@
+﻿using SocialMedia.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Core.Interface
+{
+    public interface IPostRepository
+    {
+        Task<IEnumerable<Post>> GetPosts();
+        Task<Post> GetPost(int id);
+    }
+}
---
+++ SocialMedia.Infrastructure/Repositories/PostRepositoy.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialMedia.Core.Entities;
+using SocialMedia.Core.Interface;
+using SocialMedia.Infrastructure.Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    public class PostRepositoy : IPostRepository
+    {
+        private readonly SocialMediaContext _context;
+
+        public PostRepositoy(SocialMediaContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Post>> GetPosts()
+        {
+            var posts = await _context.Posts.ToListAsync();
+            return posts;
+        }
+
+        public async Task<Post> GetPost(int id)
+        {
+            var post = await _context.Posts.FirstOrDefaultAsync(x => x.PostId == id);
+            return post;
+        }
+
+
+
+
+
+        #region Example de DB
+        //public async Task<IEnumerable<Posts>> GetPosts()
+        //{
+        //    var posts = Enumerable.Range(1, 10).Select(x => new Posts 
+        //    { 
+        //    PostId = x,
+        //    Description = $"Description {x}",
+        //    Date = DateTime.Now,
+        //    Image = $"https://misapis.com/{x}",
+        //    UserId = x*2
+        //    });
+        //    await Task.Delay(10);
+        //    return posts;
+        //}
+        #endregion
+    }
+}
